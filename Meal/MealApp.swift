e55b1d2ffdec8baf809d251e8df0c58aad541bf8
Loic D <loic.danj@gmail.com>
2022-12-06 11:23:37 +0100

    Initial Commit

---
+++ Meal/MealApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealApp.swift
+//  Meal
+//
+//  Created by Loic D on 06/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct MealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
